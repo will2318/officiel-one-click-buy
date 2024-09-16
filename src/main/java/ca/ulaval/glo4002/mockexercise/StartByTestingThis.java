729c65fd71257779aea071f102337a479673f8a4
Willy juvenal Konlack foning <willy-juvenal.konlack-foning.1@ulaval.ca>
2024-09-16 19:14:50 -0400

    étape 1 terminé

--- src/main/java/ca/ulaval/glo4002/mockexercise/StartByTestingThis.java
+++ src/main/java/ca/ulaval/glo4002/mockexercise/StartByTestingThis.java
@@ -1,15 +1,23 @@
 package ca.ulaval.glo4002.mockexercise;
 
+import ca.ulaval.glo4002.mockexercise.do_not_edit.CartFactory;
 import ca.ulaval.glo4002.mockexercise.do_not_edit.Invoice;
 
 public class StartByTestingThis {
 
+    private CartFactory cartFactory;
+
     public Invoice oneClickBuy(String clientEmail, String productSku) {
         // Étape 1 : Créer le cart avec le CartFactory
+        cartFactory.create(clientEmail);
         // Étape 2 : Trouver le produit avec le ProductRepository
         // Étape 3 : Ajouter le produit au cart
         // Étape 4 : Pour chaque item du cart, ajouter une ligne sur l'invoice
         // Étape 5 : Retourner l'invoice
         return null;
+    }
+
+    public void setCartFactorty(CartFactory cartFactory) {
+        this.cartFactory=cartFactory;
     }
 }
--- src/test/java/ca/ulaval/glo4002/mockexercise/StartByTestingThisTest.java
+++ src/test/java/ca/ulaval/glo4002/mockexercise/StartByTestingThisTest.java
@@ -1,13 +1,30 @@
 package ca.ulaval.glo4002.mockexercise;
 
+import ca.ulaval.glo4002.mockexercise.do_not_edit.CartFactory;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class StartByTestingThisTest {
 
+    public static final String UN_MAIL = "test@gmail.com";
+    public static final String UN_PRODUCK_SKU = "12345";
     private StartByTestingThis service;
+    private  CartFactory cartFactory;
 
     @BeforeEach
     public void setUp() {
         service = new StartByTestingThis();
+        cartFactory=mock(CartFactory.class);
+        service.setCartFactorty(cartFactory);
+    }
+
+    @Test
+    void givenClientEmailAndProducSkuWhenOneClickBuyThenShouldCreateCart(){
+        service.oneClickBuy(UN_MAIL, UN_PRODUCK_SKU);
+        verify(cartFactory).create(any());
     }
 }
