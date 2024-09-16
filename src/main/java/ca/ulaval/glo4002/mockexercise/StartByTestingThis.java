a18458f6745d6ec487d0bd2e1a9ff12522adb8f5
Willy juvenal Konlack foning <willy-juvenal.konlack-foning.1@ulaval.ca>
2024-09-16 19:33:01 -0400

    étape 2 terminé

--- src/main/java/ca/ulaval/glo4002/mockexercise/StartByTestingThis.java
+++ src/main/java/ca/ulaval/glo4002/mockexercise/StartByTestingThis.java
@@ -2,14 +2,18 @@ package ca.ulaval.glo4002.mockexercise;
 
 import ca.ulaval.glo4002.mockexercise.do_not_edit.CartFactory;
 import ca.ulaval.glo4002.mockexercise.do_not_edit.Invoice;
+import ca.ulaval.glo4002.mockexercise.do_not_edit.ProductRepository;
 
 public class StartByTestingThis {
 
     private CartFactory cartFactory;
+    private ProductRepository productRepository;
+    private Product product;
 
     public Invoice oneClickBuy(String clientEmail, String productSku) {
         // Étape 1 : Créer le cart avec le CartFactory
         cartFactory.create(clientEmail);
+        product=productRepository.findBySku(productSku);
         // Étape 2 : Trouver le produit avec le ProductRepository
         // Étape 3 : Ajouter le produit au cart
         // Étape 4 : Pour chaque item du cart, ajouter une ligne sur l'invoice
@@ -19,5 +23,9 @@ public class StartByTestingThis {
 
     public void setCartFactorty(CartFactory cartFactory) {
         this.cartFactory=cartFactory;
+    }
+
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository=productRepository;
     }
 }
--- src/test/java/ca/ulaval/glo4002/mockexercise/StartByTestingThisTest.java
+++ src/test/java/ca/ulaval/glo4002/mockexercise/StartByTestingThisTest.java
@@ -1,30 +1,47 @@
 package ca.ulaval.glo4002.mockexercise;
 
 import ca.ulaval.glo4002.mockexercise.do_not_edit.CartFactory;
+import ca.ulaval.glo4002.mockexercise.do_not_edit.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class StartByTestingThisTest {
 
     public static final String UN_MAIL = "test@gmail.com";
-    public static final String UN_PRODUCK_SKU = "12345";
+    public static final String SKU = "000";
+    public static final String NAME = "produit1";
+    public static final double PRICE = 30.0;
     private StartByTestingThis service;
     private  CartFactory cartFactory;
+    private ProductRepository productRepository;
 
     @BeforeEach
     public void setUp() {
         service = new StartByTestingThis();
         cartFactory=mock(CartFactory.class);
+        productRepository=mock(ProductRepository.class);
         service.setCartFactorty(cartFactory);
+        service.setProductRepository(productRepository);
     }
 
     @Test
     void givenClientEmailAndProducSkuWhenOneClickBuyThenShouldCreateCart(){
-        service.oneClickBuy(UN_MAIL, UN_PRODUCK_SKU);
+        service.oneClickBuy(UN_MAIL, SKU);
         verify(cartFactory).create(any());
+    }
+
+    @Test
+    void givenClientEmailAndProducSkuWhenOneClickBuyThenShouldFindTheProduct(){
+
+        Product product= new Product(SKU, NAME, PRICE);
+        when(productRepository.findBySku(SKU)).thenReturn(product);
+
+        service.oneClickBuy(UN_MAIL, SKU);
+
+        verify(productRepository).findBySku(SKU);
     }
 }
